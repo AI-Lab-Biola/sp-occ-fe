209576d6b0e1b37ab9ee128863e5b8af69c4349e
en20w <en.duo.wang@biola.edu>
2024-10-23 15:03:07 -0700

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Operation Christmas Child</h2>
+            <p>This page contains information about the organization.</p>
+        </div>
+    );
+};
+
+export default About;
